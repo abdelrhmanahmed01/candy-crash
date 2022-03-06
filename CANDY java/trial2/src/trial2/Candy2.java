3c200aac270e94c41db4f47c58598a5ba67076fa
abdelrahman <aa2181241@gmail.com>
2022-03-06 12:39:53 +0200

    Revert "Revert "candy_crash1""
    This reverts commit 7cb541b878c5f5add6d60acf472d3431eccbed13.

---
+++ CANDY java/trial2/src/trial2/Candy2.java
@@ -0,0 +1,12 @@
+package trial2;
+
+
+
+public class Candy2 extends Candy{
+String icon;
+
+    public Candy2(){
+        super("Lozenge",2);
+      icon ="C:\\Users\\abdelrhman\\Desktop\\CANDY java\\candies\\c22.png";
+    }
+}
---
+++ CANDY java/trial2/src/trial2/Candy3.java
@@ -0,0 +1,14 @@
+package trial2;
+
+import javax.swing.ImageIcon;
+
+public class Candy3 extends Candy{
+String icon;
+
+    public Candy3(){
+        super("Teardrop",3);
+        icon ="C:\\Users\\abdelrhman\\Desktop\\CANDY java\\candies\\c33.png";
+    }
+
+ 
+}
